551406c4e54b8ab36220a468eaffd7b1f2ca3df5
Filippo Massarelli <filippomassarelli@Filippos-MacBook-Pro-2.local>
2021-01-21 09:51:20 +0300

    create test for Campaign controller index and show actions with RSpec.

--- app/controllers/api/v1/investments_controller.rb
+++ app/controllers/api/v1/investments_controller.rb
@@ -2,6 +2,9 @@ module Api
     module V1
         class InvestmentsController < ApplicationController
 
+            before_action :find_investment, only: [:show, :update, :destroy]
+
+
             def index
                 investments = Investment.order('created_at DESC');
                 render json: {status: 'SUCCESS', message:'Loaded investments', data:investments}, status: :ok 
@@ -9,6 +12,31 @@ module Api
 
         # Here we need to definie the a the controller actions for each route
      
+            def create
+                investment = Investment.new(investment_params)
+
+                if investment.save
+                    render json: investment, status: :created
+                else
+                    render json: investment.errors, status: :unprocessable_entity
+                end   
+            end
+
+
+            private
+
+            def investment_params
+                params.permit(
+                    :user_name, 
+                    :investment_amount, 
+                    :currency
+                )
+            end
+    
+            def find_investment
+                @investment = Investment.find(params[:id])
+            end
+    
 
 
         end
---
+++ spec/requests/campaigns_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+describe 'Campaigns API', type: :request do
+
+    describe 'GET /campaigns' do
+
+        # Before this test create dummy data in test database (test database is different than development database)
+        before do
+            2.times do
+                Campaign.create({
+                    name: Faker::Company.name,
+                    image: Faker::Company.logo,
+                    percentage_raised: Faker::Number.between(from: 20, to: 200),
+                    target_amount: Faker::Number.decimal(l_digits: 6, r_digits: 2),
+                    sector: Faker::Company.industry,
+                    country: Faker::Address.country,
+                    investment_multiple: Faker::Number.decimal(l_digits: 1, r_digits: 2),
+                    currency: 'GBP',
+                    open: Faker::Boolean.boolean(true_ratio: 0.75)
+                })
+            end
+        end
+
+        it 'returns all campaigns' do
+
+            get '/api/v1/campaigns'
+
+            expect(response).to have_http_status(:success)
+            expect(response.body).to include("data")
+            expect(JSON.parse(response.body)["data"].size).to eq(2)
+        end
+    end
+
+    describe 'GET /campaign' do
+
+# Before this test also create dummy data 
+# let allows you to define variables outside the it block, thus making the actual test implementation small (easier to read)
+# bang (!) ensures that when the test first starts there is one campaign in the database
+
+        let!(:campaign) { Campaign.create({
+            name: Faker::Company.name,
+            image: Faker::Company.logo,
+            percentage_raised: Faker::Number.between(from: 20, to: 200),
+            target_amount: Faker::Number.decimal(l_digits: 6, r_digits: 2),
+            sector: Faker::Company.industry,
+            country: Faker::Address.country,
+            investment_multiple: Faker::Number.decimal(l_digits: 1, r_digits: 2),
+            currency: 'GBP',
+            open: Faker::Boolean.boolean(true_ratio: 0.75)
+        })}    
+
+    
+        it 'returns one campaign of given id' do
+            get "/api/v1/campaigns/#{campaign.id}"
+    # instead of hard coding the id (which can break in the future as tests become more complicated)
+    # use interpolation taking id of the test data variable created in the let block
+    # string interpolation requires double quotes
+            expect(response).to have_http_status(:success)
+        end
+    end
+end
